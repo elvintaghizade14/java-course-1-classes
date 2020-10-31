57aa7088222d464aa8cd110a087cd42149653541
elvintaghizade14 <elvintaghiyev184@gmail.com>
2020-10-31 17:00:33 +0400

    lesson 11 slide attached and class code added

---
+++ src/main/java/org/eminera/lesson11/lesson/HelloWorldApp.java
@@ -0,0 +1,7 @@
+package org.eminera.lesson11.lesson;
+
+public class HelloWorldApp {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/org/eminera/lesson11/lesson/MethodOverloadingApp.java
@@ -0,0 +1,39 @@
+package org.eminera.lesson11.lesson;
+
+import java.io.PrintStream;
+
+//    default olaraq javada kesr ededler double olaraq tanimlanir.
+//    double floating_point = 10.9;
+//    float floating_point2 = 10.9f;
+public class MethodOverloadingApp {
+  public static void main(String[] args) {
+    short s = 15;
+    int a = 10;
+    float f = 3.5f;
+    double d = 5.5;
+
+//    int abc;
+//    double abc;
+
+    System.out.println(vur2(s)); // short
+    System.out.println(vur2(a)); // int
+    System.out.println(vur2(f)); // float
+    System.out.println(vur2(d)); // double
+  }
+
+  public static short vur2(short eded) {
+    return (short) (eded * 2);
+  }
+
+  public static int   vur2(int eded) {
+    return eded * 2;
+  }
+
+  public static float vur2(float eded) {
+    return eded * 2;
+  }
+
+  public static double vur2(double eded) {
+    return eded * 2;
+  }
+}

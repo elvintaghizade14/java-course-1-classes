7814f2de8006d094291262372436672c04845827
elvintaghizade14 <elvintaghiyev184@gmail.com>
2020-11-03 22:21:12 +0400

    lesson 12 lesson code, hws and slides attached and class code added

---
+++ src/main/java/org/eminera/lesson12/lesson/DebugApp.java
@@ -0,0 +1,15 @@
+package org.eminera.lesson12.lesson;
+
+public class DebugApp {
+  public static void main(String[] args) {
+    int[] array = {1, 34, 56, 6, 2, 6, -2};
+    int max = array[0];
+    int min = array[0];
+    int sum = array[0];
+    for (int i = 1; i < array.length; i++) {
+      max = Math.max(max, array[i]);
+      min = Math.min(min, array[i]);
+      sum += array[i];
+    }
+  }
+}

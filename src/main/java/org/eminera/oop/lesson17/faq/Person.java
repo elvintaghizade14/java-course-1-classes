9e977e99abebc1db5fa7b8396da9aca3072a2565
elvintaghizade14 <elvintaghiyev184@gmail.com>
2020-11-25 17:09:33 +0400

    faq session

---
+++ src/main/java/org/eminera/oop/lesson17/faq/Person.java
@@ -0,0 +1,11 @@
+package org.eminera.oop.lesson17.faq;
+
+public class Person {
+  int age;
+  String name;
+
+  public Person(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+}

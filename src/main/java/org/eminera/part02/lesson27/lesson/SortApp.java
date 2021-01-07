3b9bac602afc81197ab3c966edc190114e5f64cb
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-01-07 21:48:43 +0400

    lesson #27 lesson code attached

---
+++ src/main/java/org/eminera/part02/lesson27/lesson/SortApp.java
@@ -0,0 +1,7 @@
+package org.eminera.part02.lesson27.lesson;
+
+public class SortApp {
+  public static void main(String[] args) {
+    System.out.println((0.2 + 0.2 + 0.2) * 1_000_000_000_000L);
+  }
+}

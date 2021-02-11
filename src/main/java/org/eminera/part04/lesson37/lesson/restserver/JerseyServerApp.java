04c5924ac5067e2b76372836b0a57741b1cb88e8
elvintaghizade14 <elvintaghiyev184@gmail.com>
2021-02-11 15:19:02 +0400

    lesson #37 lesson code in youtube

--- src/main/java/org/eminera/part04/lesson37/lesson/restserver/JerseyServerApp.java
+++ src/main/java/org/eminera/part04/lesson37/lesson/restserver/JerseyServerApp.java
@@ -1,5 +1,6 @@
 package org.eminera.part04.lesson37.lesson.restserver;
 
+import org.eminera.part04.lesson37.lesson.restserver.resources.BookService;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -11,10 +12,10 @@ public class JerseyServerApp {
   public static void main(String[] args) throws IOException {
     ResourceConfig config = new ResourceConfig()
             .packages("lesson37.lesson.restserver.resources");
+    config.register(BookService.class);
     HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
-            URI.create("http://localhost:8082/api/shop"),
-            config,
-            true
+            URI.create("http://localhost:8082"),
+            config
     );
   }
 }
